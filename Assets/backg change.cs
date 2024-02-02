c649069e025bf68c8a2d9469a7af54080d880780
Jesper Heese <JesperHeese@outlook.com>
2024-02-02 11:03:56 +0100

    update

--- Assets/backg change.cs
+++ Assets/backg change.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class backgchange : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Camera main;
+    [SerializeField]
+    private GameObject playerpos;
+    private float timeleft;
+
+    private float height = 1065f;
+
     void Start()
     {
-        
+        main = GetComponent<Camera>();
+        playerpos = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timeleft -= Time.deltaTime;
+        backgroundcolor();
+    }
+
+    private void backgroundcolor()
+    {
+        if (playerpos.transform.position.y >= height)
+        {
+
+            main.backgroundColor = new Color(0f, 0.1185984f, 0.3113208f);
+        }
+        if (playerpos.transform.position.y <= height)
+        {
+
+            main.backgroundColor = new Color(0.6367924f, 0.7780398f, 1f);
+        }
+
+
     }
 }
